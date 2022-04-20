d859735c87fa5c9ac763edb09dd92a8d91d3fc73
sam bacha <sam@manifoldfinance.com>
2022-04-20 10:29:30 -0700

    getBaseFeeInNextBlock

---
+++ src/utils/getBaseFeeInNextBlock.ts
@@ -0,0 +1,31 @@
+import { BigNumber } from 'ethers'
+
+const BASE_FEE_MAX_CHANGE_DENOMINATOR = 8
+
+export const getBaseFeeInNextBlock = (
+  currentBaseFeePerGas: BigNumber,
+  currentGasUsed: BigNumber,
+  currentGasLimit: BigNumber
+): BigNumber => {
+  const currentGasTarget = currentGasLimit.div(2)
+
+  if (currentGasUsed.eq(currentGasTarget)) {
+    return currentBaseFeePerGas
+  } else if (currentGasUsed.gt(currentGasTarget)) {
+    const gasUsedDelta = currentGasUsed.sub(currentGasTarget)
+    const baseFeePerGasDelta = currentBaseFeePerGas
+      .mul(gasUsedDelta)
+      .div(currentGasTarget)
+      .div(BASE_FEE_MAX_CHANGE_DENOMINATOR)
+
+    return currentBaseFeePerGas.add(baseFeePerGasDelta)
+  } else {
+    const gasUsedDelta = currentGasTarget.sub(currentGasUsed)
+    const baseFeePerGasDelta = currentBaseFeePerGas
+      .mul(gasUsedDelta)
+      .div(currentGasTarget)
+      .div(BASE_FEE_MAX_CHANGE_DENOMINATOR)
+
+    return currentBaseFeePerGas.sub(baseFeePerGasDelta)
+  }
+}
